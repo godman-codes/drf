90b5532d607f71be30ab566f9d54f4a1fcee114d
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-08-17 20:45:57 +0100

    py client

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,6 @@ import requests
 
 end_point = "http://localhost:8000/api/"
 
-response = requests.get(end_point)
+response = requests.get(end_point, json={'query': 'hello world'})
 
 print(response.json())
