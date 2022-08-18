ecada7a31665147ecb4f3ebce0d90be8529822ac
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-08-18 21:11:58 +0100

    making the GENERIC CLASS handle two methods post for create and get for list

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import product_detail_view, product_create_view, product_Update_view
+# from .views import product_detail_view, product_create_view, product_Update_view
+from .views import product_detail_view, product_list_create_view, product_Update_view
 
 urlpatterns = [
     path('<int:pk>/', product_detail_view),
-    path('', product_create_view),
+    path('', product_list_create_view),
     path('<int:pk>/update/', product_Update_view)
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -13,9 +13,36 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 
 product_detail_view = ProductDetailAPIView.as_view()
 
-class ProductCreateAPIView(generics.CreateAPIView):
+# class ProductCreateAPIView(generics.CreateAPIView):
+#     '''
+#     create products
+#     '''
+#     queryset = Product.objects.all() #getting the query sets from the database
+#     serializer_class = ProductSerializers
+
+#     def perform_create(self, serializer):
+#         '''
+#         this function can only be called in the generic create class view
+#         and we can use it to manipulate the data we want to save to the data base
+#         '''
+#         # serializer.save(user=self.request.user) we can assign user like this 
+#         print(serializer.validated_data)
+#         title = serializer.validated_data.get('title') # this gets the title from the from the validated serialized data
+#         content = serializer.validated_data.get('content') or None # this get the content and if the content isn't there it will return None  
+
+#         if not content:
+#             content = title # if the content is not there then we will assign the title to the content
+#         serializer.save(content=content) # we can save the content to the database
+
+# product_create_view = ProductCreateAPIView.as_view() # this is the view that will be used in the urls.py file
+
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     '''
-    cerate view
+    if request.method == 'POST':
+        we will create a new product
+    if request.method == 'GET':
+        we will get all the products as a list
     '''
     queryset = Product.objects.all() #getting the query sets from the database
     serializer_class = ProductSerializers
@@ -34,12 +61,13 @@ class ProductCreateAPIView(generics.CreateAPIView):
             content = title # if the content is not there then we will assign the title to the content
         serializer.save(content=content) # we can save the content to the database
 
-product_create_view = ProductCreateAPIView.as_view() # this is the view that will be used in the urls.py file
+product_list_create_view = ProductListCreateAPIView.as_view() # this is the view that will be used in the urls.py file
+
 
 
 class ProductUpdateAPIView(generics.UpdateAPIView):
     '''
-    cerate view
+    create view
     '''
     queryset = Product.objects.all() # getting the query sets from the database
     serializer_class = ProductSerializers # this is the serializer class that will be used to serialize the data
