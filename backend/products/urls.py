e5113a3bf196b75cde27051a9cfce2d400c0d802
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-08-18 22:10:19 +0100

    going back to the class base views

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 # from .views import product_detail_view, product_create_view, product_Update_view
-from .views import product_detail_view, product_list_create_view, product_Update_view, product_alt_view
+from .views import product_detail_view, product_list_create_view, product_Update_view
 
 urlpatterns = [
-    path('<int:pk>/', product_alt_view),
-    path('', product_alt_view),
-    # path('<int:pk>/update/', product_Update_view)
+    path('<int:pk>/', product_detail_view),
+    path('', product_list_create_view),
+    path('<int:pk>/update/', product_Update_view)
 ]
