311911552d01eb08fbf85e24b64e347ea441b8d2
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-08-24 13:26:44 +0100

    py_client

--- py_client/details.py
+++ py_client/details.py
@@ -4,7 +4,7 @@ from login import authLogin
 end_point = "http://localhost:8000/api/product/7/"
 token = authLogin()
 if token is not None:
-    response = requests.get(end_point, headers={"Authorization": f"Token {token}"})
+    response = requests.get(end_point, headers={"Authorization": f"Bearer {token}"})
     print(response.json())
 else:
     raise Exception("invalid credentials")
