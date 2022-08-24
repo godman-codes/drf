caadcbe4903ef06ebe6f2cd2641099c6710f9371
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-08-25 00:56:39 +0100

    creating a user in the product model with foreign key to the django user model

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL # just a string to auth.user
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, related_name="products", default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=15, default=99.99)
