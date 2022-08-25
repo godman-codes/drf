f0277642a1b2e42e24b72e50d62f0f6c43ad50cd
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-08-25 14:10:26 +0100

    creating a custom query set for users that own objects

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -1,8 +1,24 @@
-from .permissions import IsStaffEditorPermission, IsDeleteRolesPermission
+from .permissions import IsStaffEditorPermission
 from rest_framework import permissions
 
 class StaffEditorPermissionMixin():
-    permission_classes = [permissions.IsAdminUser ,IsStaffEditorPermission]
+    # print('cold mixins')
+    permission_classes = [IsStaffEditorPermission]
 
-class DeleteRolesPermissionMixin():
-    permission_classes = [IsDeleteRolesPermission]
+# class DeleteRolesPermissionMixin():
+#     permission_classes = [IsDeleteRolesPermission]
+
+
+class UserQuerySetMixin():
+    user_field = 'user'
+    allow_staff_view = False
+    def get_queryset(self, *args, **kwargs):
+        user = self.request.user
+        lookup_data = {}
+        lookup_data[self.user_field] = user
+        # print(lookup_data)
+        qs = super().get_queryset(*args, **kwargs)
+        # print(qs)
+        if self.allow_staff_view and user.is_staff:
+            return qs
+        return qs.filter(**lookup_data)
