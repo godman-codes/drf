ff937176a8e60bfa80cf321ab84058038882ffaa
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-09-02 14:00:52 +0100

    created the search view class

--- backend/search/views.py
+++ backend/search/views.py
@@ -3,6 +3,18 @@ from products.models import Product
 from products.serializers import ProductSerializers
 
 class SearchListView(generics.ListCreateAPIView):
-    queryset = Product.object.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializers
-    
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        q = self.request.GET.get('q') # this gets the query parameter aka the search parameter from the request object
+        results = Product.objects.none() # this return if the query parameter is not specified
+        if q is not None:
+            user = None # by default without authentication the user is none
+            if self.request.user.is_authenticated:
+                user  = self.request.user # assign the user to the user variable
+            results = qs.search(q, user=user) # this is the method defined in the Product manager class
+        return results
+
+search_list_view = SearchListView.as_view()
